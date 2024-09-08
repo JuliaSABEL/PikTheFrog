661152d9c3bfe3a03dc032063e572b4e6e19abe3
JuliaSABEL <sabelnikova.j.partner@gmail.com>
2024-09-08 23:10:46 +0300

    Simple movement
    A simple movement script was added to the frog, the camera now follows it, and an idle animation was also added.

--- Pik The Frog/Assets/Scripts/FrogController.cs
+++ Pik The Frog/Assets/Scripts/FrogController.cs
@@ -1,47 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FrogController : MonoBehaviour
 {
+    [SerializeField]
     private float frogSpeed = 3f;
-
-    public int toadHeart = 3;
-
-    public bool isInvulnerabilityState;
 
     private float horizontalMove, verticalMove;
 
-    public Rigidbody2D frogRigidBody;
+    private Rigidbody2D frogRigidBody;
 
     void Start()
     {
         frogRigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    public void FrogControll()
+    void FixedUpdate()
     {
         if (horizontalMove != 0 || verticalMove != 0)
         {
             Vector2 movement = new Vector2(horizontalMove * frogSpeed, verticalMove * frogSpeed);
             frogRigidBody.velocity = movement;
-        }
+    }
         else
         {
             frogRigidBody.velocity = Vector2.zero;
         }
     }
 
-    public void PlayerInput()
+    void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal");
 
         verticalMove = Input.GetAxis("Vertical");
-    }
-
-    private void FixedUpdate()
-    {
-        FrogControll();
-        PlayerInput();
     }
 }
